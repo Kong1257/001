68c0af994372dbab432726b7c4f4591d93e02ad1
Kong1257 <56631747+Kong1257@users.noreply.github.com>
2025-08-29 14:50:54 +0800

    diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,15 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+chmod -R 755 ./package/luci-app-adguardhome/*
+git clone https://github.com/SunBK201/UA3F.git package/UA3F
+chmod -R 755 ./package/UA3F/*
+git clone https://github.com/CHN-beta/rkp-ipid.git package/rkp-ipid
+chmod -R 755 ./package/rkp-ipid/*
+git clone https://github.com/linkease/istore.git package/istore
+chmod -R 755 ./package/istore/*
+git clone https://github.com/Toolcen/luci-app-sms-tool.git package/luci-app-sms-tool
+chmod -R 755 ./package/luci-app-sms-tool/*
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
+chmod -R 755 ./package/mentohust/*
